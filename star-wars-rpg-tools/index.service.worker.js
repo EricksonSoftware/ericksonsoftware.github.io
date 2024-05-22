2b6f2f41af812e2496f756df5e2d3ea2df4270bd
MataclysmGames <matterickson915@gmail.com>
2024-05-22 16:22:16 -0700

    star wars tool update

--- star-wars-rpg-tools/index.service.worker.js
+++ star-wars-rpg-tools/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1716419302|4152613';
+const CACHE_VERSION = '1716420104|4124487';
 /** @type {string} */
 const CACHE_PREFIX = 'Star-Wars-RPG-To-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
