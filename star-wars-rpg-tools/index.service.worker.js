f56c84653c55d1abe98484bb9f7ff5d0de75f8a0
MataclysmGames <matterickson915@gmail.com>
2024-05-22 14:36:30 -0700

    common or exotic random

--- star-wars-rpg-tools/index.service.worker.js
+++ star-wars-rpg-tools/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1716407474|4182150';
+const CACHE_VERSION = '1716413656|4183227';
 /** @type {string} */
 const CACHE_PREFIX = 'Star-Wars-RPG-To-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
