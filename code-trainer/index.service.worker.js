1ff86892a040253f941230b578c0b09f96e6f97f
MataclysmGames <matterickson915@gmail.com>
2024-07-14 22:34:31 -0700

    update code-trainer

--- code-trainer/index.service.worker.js
+++ code-trainer/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1720745270|23822805117';
+const CACHE_VERSION = '1721021611|4896395';
 /** @type {string} */
 const CACHE_PREFIX = 'Code-Trainer-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
