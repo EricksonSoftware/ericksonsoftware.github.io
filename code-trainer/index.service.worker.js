93ecbcd184778fb84649d6bed2470578d1780324
MataclysmGames <matterickson915@gmail.com>
2024-07-14 22:37:55 -0700

    code-trainer: don't install languages on web

--- code-trainer/index.service.worker.js
+++ code-trainer/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1721021611|4896395';
+const CACHE_VERSION = '1721021840|4904220';
 /** @type {string} */
 const CACHE_PREFIX = 'Code-Trainer-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
